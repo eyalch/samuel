d58750e2200e0e9c1dcb25af1c5309c903e2dff5
Eyal Cherevatzki <eyal.3555@protonmail.com>
2019-10-07 21:31:17 +0300

    Use Postgres

--- server/settings.py
+++ server/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "apps.users",
     "apps.dishes",
 ]
@@ -77,8 +78,12 @@ WSGI_APPLICATION = "server.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "samuel",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
@@ -125,4 +130,5 @@ AUTH_USER_MODEL = "users.User"
 # Media files
 
 MEDIA_ROOT = "media/"
+
 MEDIA_URL = "/media/"

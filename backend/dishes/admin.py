be7e0f67feab052b30f04467bfc6e7528269fbeb
Eyal Cherevatzki <eyal.3555@protonmail.com>
2019-11-21 00:51:16 +0200

    feat(admin): add a "mark dishes for today" action

--- backend/dishes/admin.py
+++ backend/dishes/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils import timezone
 
 from .models import Dish, Order
 
@@ -6,6 +7,27 @@ from .models import Dish, Order
 @admin.register(Dish)
 class DishAdmin(admin.ModelAdmin):
     list_display = ("name", "description", "next_date", "image")
+    actions = ["mark_for_today"]
+
+    def mark_for_today(self, request, queryset):
+        today = timezone.now()
+
+        # Iterate through the selected dished and update only the date
+        # (while keeping the original time)
+        for dish in queryset:
+            dish.next_date = dish.next_date.replace(
+                year=today.year, month=today.month, day=today.day
+            )
+            dish.save()
+
+        rows_updated = len(queryset)
+        if rows_updated == 1:
+            message_bit = "1 dish was"
+        else:
+            message_bit = f"{rows_updated} dishes were"
+        self.message_user(request, f"{message_bit} marked for today.")
+
+    mark_for_today.short_description = "Mark selected dishes for today"
 
 
 @admin.register(Order)

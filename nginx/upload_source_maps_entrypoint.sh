10b9ef64bf5975cd7344f8b0a4b4a261697c574a
Eyal Cherevatzki <eyal_c@mintapp.co.il>
2020-02-12 18:47:51 +0200

    fix: use .env in nginx for source maps script

--- nginx/upload_source_maps_entrypoint.sh
+++ nginx/upload_source_maps_entrypoint.sh
@@ -6,7 +6,7 @@ for source_map_filename in static/js/*.map; do
         minified_url=$BASE_URL/${source_map_filename%.*}
 
         curl https://api.rollbar.com/api/1/sourcemap \
-                -F access_token=$ROLLBAR_POST_TOKEN \
+                -F access_token=$ROLLBAR_TOKEN \
                 -F version=$VERSION \
                 -F minified_url=$minified_url \
                 -F source_map=@$source_map_filename

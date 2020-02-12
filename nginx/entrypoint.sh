043fe3818dc17c980dc78b0b271d8665123a5556
Eyal Cherevatzki <eyal_c@mintapp.co.il>
2020-02-12 20:04:09 +0200

    chore: improve entrypoint script

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -2,14 +2,16 @@
 
 cd /usr/share/nginx/html/
 
+echo "Uploading source maps..."
 for source_map_filename in static/js/*.map; do
         minified_url=$BASE_URL/${source_map_filename%.*}
 
-        curl https://api.rollbar.com/api/1/sourcemap \
+        curl --silent --show-error https://api.rollbar.com/api/1/sourcemap \
                 -F access_token=$ROLLBAR_TOKEN \
                 -F version=$VERSION \
                 -F minified_url=$minified_url \
                 -F source_map=@$source_map_filename
 done
+echo "Done uploading source maps."
 
-exec "$@"
+nginx -g "daemon off;"
